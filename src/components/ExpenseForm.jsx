a902e2ef629a82b5ff15d763f8c453bc5c65f623
Nishu singh <nishu.frontend@gmail.com>
2026-03-12 13:24:22 +0530

    fix attribute in ExpenseForm component

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -2,20 +2,20 @@
 
 function ExpenseForm() {
   return (
-      <form class="expense-form">
-          <div class="input-container">
-            <label for="title">Title</label>
+      <form className="expense-form">
+          <div className="input-container">
+            <label htmlFor="title">Title</label>
             <input id="title" />
           </div>
-          <div class="input-container">
-            <label for="category">Category</label>
+          <div className="input-container">
+            <label htmlFor="category">Category</label>
             <input id="category" />
           </div>
-          <div class="input-container">
-            <label for="amount">Amount</label>
+          <div className="input-container">
+            <label htmlFor="amount">Amount</label>
             <input id="amount" />
           </div>
-          <button class="add-btn">Add</button>
+          <button className="add-btn">Add</button>
         </form>
   )
 }
